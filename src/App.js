d2138ddd29d629c0416733f77d4fc4784ddc7618
Samantha Marie <sdurazo.UNLV@gmail.com>
2023-07-23 11:04:27 -0700

    formatted API Call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import './App.css';
+import Gallery from './Gallery'
+import ButtonBar from './ButtonBar'
+import { useState, useEffect } from "react";
 
 function App() {
+let [date, setData] = useState({})
+let [artId, setArtId] = useState(350000)
+
+  {/* State variables here... */}
+  useEffect (() => {
+    document.title = 'Welcome to ArtWorld!'
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
+    .then(response => response.json())
+    .then(resData => setData(resData))
+  }, [artId])
+
+  {/* Return to JSX down here... */}
+
   return (
     <div className="App">
-      
+      <Gallery />
+      <ButtonBar />
     </div>
   );
 }
---
+++ src/ButtonBar.js
@@ -0,0 +1,5 @@
+export default function ButtonBar() {
+    return (
+        <h1>ButtonBar</h1>
+    )
+}
